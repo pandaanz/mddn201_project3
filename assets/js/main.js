976657b80b7c54166d49b1f9d0f62883768ec808
pandaanz <37090772+pandaanz@users.noreply.github.com>
2018-09-11 08:40:51 +0700

    edited location

--- assets/js/main.js
+++ assets/js/main.js
@@ -606,8 +606,8 @@ console.log('%c Proudly Crafted with ZiOn.', 'background: #222; color: #bada55')
         // When the window has finished loading create our google map below
         google.maps.event.addDomListener(window, 'load', init);
 
-        var mkr = new google.maps.LatLng(-41.294846, 174.776203);
-        var cntr = (mobileTest) ? mkr : new google.maps.LatLng(-41.294846, 174.776203);
+        var mkr = new google.maps.LatLng(-41.2953356, 174.7756338,19z);
+        var cntr = (mobileTest) ? mkr : new google.maps.LatLng(-41.2953356, 174.7756338,19z);
 
         function init() {
             // Basic options for a simple Google Map
